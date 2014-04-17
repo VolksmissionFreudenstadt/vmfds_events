92277067bfeda588fd7b23ca53c6acfdf7446765
Christoph Fischer <chris@toph.de>
2014-04-17 15:05:09 +0200

    Corrected namespace of DateTime object

--- Classes/Controller/EventController.php
+++ Classes/Controller/EventController.php
@@ -44,30 +44,46 @@ class EventController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 		$this->koolEventRepository = new \VMFDS\VmfdsEvents\Domain\Repository\KoolEventRepository();
 	}
 	
+	private function array_merge_recursive_distinct (array $array1, array $array2 ) {
+	  $merged = $array1;
+	  foreach ( $array2 as $key => &$value ) {
+	    if ( is_array ( $value ) && isset ( $merged [$key] ) && is_array ( $merged [$key] ) ) {
+	      $merged [$key] = $this->array_merge_recursive_distinct ( $merged [$key], $value );
+	    } else {
+	      $merged [$key] = $value;
+	    }
+	  }
+	  return $merged;
+	}
+	
 	public function importFlexFormSettings() {
 		$additionalTS = $this->settings['myTS'];
 		if ($additionalTS) {
-			unset($this->settings['myTS']);
 			if ($additionalTS) {
+				unset($this->settings['myTS']);
 				$tsParser = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('t3lib_TSparser');
-				$tsParser->setup = array('settings.' => $this->settings);
+				$tsParser->setup = array();
 				$tsParser->parse(trim($additionalTS));
 				$tmp = $tsParser->setup;
 			}
 			$tmp = $this->tsRemoveDots($tmp);
-			$this->settings = $tmp['settings'];
+			$tmp = $this->array_merge_recursive_distinct($this->settings, $tmp);
+			$this->settings = $tmp;
 		}
 	}
 		
 	public function listAction() {
 		$this->importFlexFormSettings();
 		$events = $this->koolEventRepository->findBySettings($this->settings, $this->request->getArguments());
+		$this->view->assign('settings', $this->settings);
 		$this->view->assign('events', $events);
+		//die ('<pre>'.print_r($this->settings,1));
 	}
 	
 	public function showAction() {
 		$this->importFlexFormSettings();
 		$req = $this->request->getArguments();
+		$this->view->assign('settings', $this->settings);
 		$this->view->assign('event', $this->koolEventRepository->findByUid($req['id']));
 	}
 
